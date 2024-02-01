89d1820029c78050fe0959d4a668affe1ca8ab98
Paul Rusch <pauly929@hotmail.de>
2024-02-01 21:48:50 +0100

    chore: relativ path imports

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "src": fileURLToPath(new URL("./src/", import.meta.url)),
     },
   },
   test: vitestConfig.test,
