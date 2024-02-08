14383486c26153d202a2f875fbae4fad03507f3f
Paul Rusch <pauly929@hotmail.de>
2024-02-08 21:52:37 +0100

    style: icons are set up

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: "mdi",
+  },
+  theme: {
+    defaultTheme: "dark"
+  }
 });
 
 createApp(App).use(vuetify).mount("#app");
